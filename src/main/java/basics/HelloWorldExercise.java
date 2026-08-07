7cb218b1ccf91a2cf8c46a158a6e2238e712218c
Viktoryia <kustava.viktoryia@gmail.com>
2026-08-07 23:21:46 +0300

    Migrate project to Gradle and add JUnit setup

--- src/main/java/basics/HelloWorldExercise.java
+++
@@ -1,9 +0,0 @@
-package basics;
-
-public class HelloWorldExercise {
-
-  public static void main(String[] args) {
-    System.out.println("Hello World");
-  }
-
-}
---
+++ src/test/java/examples/CalculatorTest.java
@@ -0,0 +1,29 @@
+package examples;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculatorTest {
+  private Calculator calculator;
+  @BeforeEach
+  void setUp(){
+    calculator=new Calculator();
+  }
+@Test
+  void shouldSumTwoPositiveNumbers(){
+  //Calculator calculator=new Calculator();
+  int result=calculator.sum(2,3);
+assertEquals(5, result);
+}
+@Test
+  void shouldSumTwoPositiveNumbers1(){
+    int result=calculator.sum(4,8);
+    assertEquals(12,result);
+}
+
+  @Test
+  void shouldSumWithZero() {
+    int result = calculator.sum(0, 2);
+    assertEquals(2, result);
+  }
+}
